47d91dc13bbc7a7d481a99f25931a70331cf0c8a
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-12-10 12:16:51 +0000

    Update app.py

--- Part2-RAG-Sales-Manual/RAG-Loader/app.py
+++ Part2-RAG-Sales-Manual/RAG-Loader/app.py
@@ -10,7 +10,7 @@ import os
 import logging
 
 app = Flask(__name__)
-CORS(app, origins=["https://rag-webpage-deploy-llm-test-project.apps.cyan.pssc.mop.fr.ibm.com"]) 
+CORS(app, origins=["https://rag-webpage-deploy-llm-test-project.apps.p1370.cecc.ihost.com"]) 
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
